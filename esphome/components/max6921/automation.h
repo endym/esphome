9513ac8b73acd32c32e64c177e7bb2fa3e9023dc
endym <endym@mailbox.org>
2026-02-12 19:14:36 +0100

    [max6921] fix compile error with esphome 2026.1
    void esphome::max6921::SetDemoModeAction<Ts>::play(Ts ...) marked 'override', but does not override

--- esphome/components/max6921/automation.h
+++ esphome/components/max6921/automation.h
@@ -25,7 +25,7 @@ template<typename... Ts> class SetTextAction : public Action<Ts...> {
   TEMPLATABLE_VALUE(uint32_t, text_repeat_interval)
   TEMPLATABLE_VALUE(uint32_t, text_effect_update_interval)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     auto pos = this->text_position_.value(x...);
     auto effect_cycle_num = this->text_effect_cycle_num_.value(x...);
     auto duration = this->text_effect_duration_.value(x...);
@@ -49,7 +49,7 @@ template<typename... Ts> class SetDemoModeAction : public Action<Ts...> {
   TEMPLATABLE_VALUE(uint32_t, demo_update_interval)
   TEMPLATABLE_VALUE(uint8_t, demo_cycle_num)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     auto update_interval = this->demo_update_interval_.value(x...);
     auto cycle_num = this->demo_cycle_num_.value(x...);
     this->max9621_->display_->set_demo_mode(this->mode_.value(x...), update_interval, cycle_num);
